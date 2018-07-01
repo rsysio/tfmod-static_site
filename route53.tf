b98c31e54ba4f4c0d31d7142a76bc154a4af4bfa
torz <torz@gmx.com>
2018-07-01 17:24:27 +0100

    fixed bucket policy

--- route53.tf
+++ route53.tf
@@ -2,11 +2,10 @@ resource "aws_route53_record" "site" {
   zone_id = "${var.zone_id}"
   name    = "${var.domain_name}"
   type    = "A"
-  ttl     = "300"
 
   alias {
     name                   = "${aws_cloudfront_distribution.site.domain_name}"
-    zone_id                = "${var.zone_id}"
+    zone_id                = "Z2FDTNDATAQYW2"
     evaluate_target_health = false
   }
 }
--- s3.tf
+++ s3.tf
@@ -1,15 +1,13 @@
 resource "aws_s3_bucket" "site" {
   acl = "private"
 
-  policy = "${data.aws_iam_policy_document.s3_policy.json}"
-
   tags = "${merge(
     var.default_tags,
     map("Role", "static site")
   )}"
 }
 
-data "aws_iam_policy_document" "s3_policy" {
+data "aws_iam_policy_document" "bucket" {
   statement {
     actions   = ["s3:GetObject"]
     resources = ["${aws_s3_bucket.site.arn}/*"]
@@ -19,4 +17,9 @@ data "aws_iam_policy_document" "s3_policy" {
       identifiers = ["arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity ${var.origin_access_id}"]
     }
   }
+}
+
+resource "aws_s3_bucket_policy" "site" {
+  bucket = "${aws_s3_bucket.site.id}"
+  policy = "${data.aws_iam_policy_document.bucket.json}"
 }
